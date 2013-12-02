4ca1703d41d649b32e6978f67773261db041ed5a
Fabio Lima Santos <flsusp@gmail.com>
2013-12-02 14:23:21 -0200

    Adding inline value expression factory

--- src/main/java/br/com/datastore/Expression.java
+++ src/main/java/br/com/datastore/Expression.java
@@ -7,4 +7,8 @@ public abstract class Expression {
 	public abstract String toString();
 
 	protected abstract void populateParameters(List<Object> parameters);
+
+	public static Expression inline(String value) {
+		return new InlineValueExpression(value);
+	}
 }
